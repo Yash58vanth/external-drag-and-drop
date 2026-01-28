17adfc1e440de86615cb467575a80e1f13b7f126
yash58vanth <yashvanth.ganesan@syncfusion.com>
2026-01-28 20:31:54 +0530

    Updated demo

---
+++ assets/customization.stories-COUts0qz.js
@@ -0,0 +1,144 @@
+import{j as e}from"./index-7s7xaQU8.js";import{r as b}from"./index-CC6F48bw.js";import{S as o,D as d,W as u,a as m,M as w,f as ue,T as O,V as N}from"./month-view-8qfidW35.js";import{s as l,r as me}from"./dataSource-CrVxx1x3.js";/* empty css               */import"./index-qiR4Kxfn.js";const he=({interval:n=60,slotCount:t=2,enable:a=!0})=>{const c={dataSource:l};return e.jsx("div",{className:"control-section",children:e.jsx("div",{className:"scheduler-control",children:e.jsxs(o,{height:"550px",width:"100%",defaultSelectedDate:new Date(2021,0,10),eventSettings:c,timeScale:{interval:n,slotCount:t,enable:a},children:[e.jsx(d,{}),e.jsx(u,{}),e.jsx(m,{})]})})})},A=[{text:"Mon, Wed, Fri",value:[1,3,5]},{text:"Mon, Tue, Wed, Thu, Fri",value:[1,2,3,4,5]},{text:"Tue, Wed, Thu, Fri",value:[2,3,4,5]},{text:"Thu, Fri, Sat, Mon, Tue",value:[4,5,6,1,2]},{text:"Tue, Thu",value:[2,4]}],L=[{text:"Sunday",value:0},{text:"Monday",value:1},{text:"Tuesday",value:2},{text:"Wednesday",value:3},{text:"Thursday",value:4},{text:"Friday",value:5},{text:"Saturday",value:6}],pe=({showWeekend:n,workingDays:t,firstDayOfWeek:a})=>{var x;const h=(A.find(i=>i.text===t)??A[0]).value,p=((x=L.find(i=>i.text===a))==null?void 0:x.value)??0,g={dataSource:l};return e.jsx("div",{className:"control-section",children:e.jsx("div",{className:"scheduler-control",children:e.jsxs(o,{height:"550px",width:"100%",defaultSelectedDate:new Date(2021,0,10),eventSettings:g,workDays:h,showWeekend:n,firstDayOfWeek:p,children:[e.jsx(d,{}),e.jsx(u,{}),e.jsx(m,{}),e.jsx(w,{})]})})})},ge=({showHeaderBar:n})=>{const t={dataSource:l};return e.jsx("div",{className:"control-section",children:e.jsx("div",{className:"scheduler-control",children:e.jsxs(o,{height:"550px",width:"100%",defaultSelectedDate:new Date(2021,0,10),eventSettings:t,showHeaderBar:n,children:[e.jsx(d,{}),e.jsx(u,{}),e.jsx(m,{}),e.jsx(w,{})]})})})},xe=({highlight:n})=>{const t={dataSource:l},[a,c]=b.useState(new Date(2025,1,14,9,0)),[h,p]=b.useState(new Date(2025,1,14,18,0)),g=s=>s?ue(s,{format:"HH:mm"}):void 0,x=s=>{const B=s.value?new Date(s.value):null;c(B)},i=s=>{const B=s.value?new Date(s.value):null;p(B)},C=g(a),r=g(h);return e.jsxs(e.Fragment,{children:[e.jsxs("div",{className:"sf-content-center gap",children:[e.jsx(O,{value:a,onChange:x,variant:N.Outlined,clearButton:!1,editable:!1,placeholder:"From",labelMode:"Always",style:{width:"150px"}}),e.jsx(O,{value:h,minTime:a,onChange:i,variant:N.Outlined,clearButton:!1,editable:!1,placeholder:"To",labelMode:"Always",style:{width:"150px"}})]}),e.jsx("div",{className:"control-section",children:e.jsx("div",{className:"scheduler-control",children:e.jsxs(o,{height:"550px",width:"100%",defaultSelectedDate:new Date(2021,0,10),eventSettings:t,workHours:{highlight:n,start:C,end:r},children:[e.jsx(d,{}),e.jsx(u,{}),e.jsx(m,{})]})})})]})},we=({})=>{const n={dataSource:l},[t,a]=b.useState(new Date(2025,1,14,9,0)),[c,h]=b.useState(new Date(2025,1,14,18,0)),p=r=>r?ue(r,{format:"HH:mm"}):void 0,g=r=>{const s=r.value?new Date(r.value):null;a(s)},x=r=>{const s=r.value?new Date(r.value):null;h(s)},i=p(t),C=p(c);return e.jsxs(e.Fragment,{children:[e.jsxs("div",{className:"sf-content-center gap",children:[e.jsx(O,{value:t,onChange:g,variant:N.Outlined,clearButton:!1,editable:!1,placeholder:"From",labelMode:"Always",style:{width:"150px"}}),e.jsx(O,{value:c,minTime:t,onChange:x,variant:N.Outlined,clearButton:!1,editable:!1,placeholder:"To",labelMode:"Always",style:{width:"150px"}})]}),e.jsx("div",{className:"control-section",children:e.jsx("div",{className:"scheduler-control",children:e.jsxs(o,{height:"550px",width:"100%",defaultSelectedDate:new Date(2021,0,10),eventSettings:n,startHour:i,endHour:C,children:[e.jsx(d,{}),e.jsx(u,{}),e.jsx(m,{})]})})})]})},je=({})=>e.jsx("div",{className:"control-section",children:e.jsx("div",{className:"scheduler-control",children:e.jsxs(o,{className:"custom-scheduler",height:"550px",width:"100%",defaultSelectedDate:new Date(2021,0,10),children:[e.jsx(d,{}),e.jsx(u,{}),e.jsx(m,{})]})})}),Se=({})=>{const n={dataSource:me};return e.jsx("div",{className:"control-section",children:e.jsx("div",{className:"scheduler-control",children:e.jsxs(o,{height:"550px",width:"100%",defaultSelectedDate:new Date,eventSettings:n,children:[e.jsx(d,{}),e.jsx(u,{}),e.jsx(m,{})]})})})},De=({numberOfWeeks:n})=>{const t={dataSource:l};return e.jsx("div",{className:"control-section",children:e.jsx("div",{className:"scheduler-control",children:e.jsx(o,{height:"550px",width:"100%",defaultSelectedDate:new Date(2021,0,10),eventSettings:t,defaultView:"Month",children:e.jsx(w,{numberOfWeeks:n})})})})},ve=({showTrailingAndLeadingDates:n})=>{const t={dataSource:l};return e.jsx("div",{className:"control-section",children:e.jsx("div",{className:"scheduler-control",children:e.jsx(o,{height:"550px",width:"100%",defaultSelectedDate:new Date(2021,0,10),eventSettings:t,defaultView:"Month",children:e.jsx(w,{showTrailingAndLeadingDates:n})})})})},ye=({rowAutoHeight:n,ignoreWhitespace:t})=>{const a={dataSource:l,ignoreWhitespace:t};return e.jsx("div",{className:"control-section",children:e.jsx("div",{className:"scheduler-control",children:e.jsx(o,{height:"550px",width:"100%",defaultSelectedDate:new Date(2021,0,10),eventSettings:a,defaultView:"Month",rowAutoHeight:n,children:e.jsx(w,{})})})})},Oe={title:"Scheduler/Customization",parameters:{layout:"padded",docs:{description:{component:"Meta configuration for the Storybook stories"}}},tags:["autodocs"]},j={render:n=>e.jsx(he,{...n}),args:{interval:60,slotCount:2,enable:!0},parameters:{controls:{include:["interval","slotCount","enable"]}},argTypes:{interval:{control:"select",options:[30,60,90,120,150,180,240,300,720]},slotCount:{control:"select",options:[1,2,3,4,5,6]},enable:{console:"boolean"}}},S={render:n=>e.jsx(ge,{...n}),args:{showHeaderBar:!0},parameters:{controls:{include:["showHeaderBar"]}},argTypes:{showHeaderBar:{console:"boolean"}}},D={render:n=>e.jsx(pe,{...n}),args:{workingDays:A[0].text,firstDayOfWeek:L[0].text,showWeekend:!0},parameters:{controls:{include:["workingDays","firstDayOfWeek","showWeekend"]}},argTypes:{workingDays:{control:"select",options:A.map(n=>n.text)},firstDayOfWeek:{control:"select",options:L.map(n=>n.text)},showWeekend:{control:"boolean"}}},v={render:n=>e.jsx(xe,{...n}),args:{highlight:!0},parameters:{controls:{include:["highlight","start","end"]}},argTypes:{highlight:{control:"boolean"}}},y={render:n=>e.jsx(we,{...n}),parameters:{controls:{include:["startHour","endHour"]}}},f={render:n=>e.jsx(De,{...n}),args:{numberOfWeeks:6},parameters:{controls:{include:["numberOfWeeks"]}},argTypes:{numberOfWeeks:{control:"select",options:[1,2,3,4,5,6]}}},k={render:n=>e.jsx(ve,{...n}),args:{showTrailingAndLeadingDates:!0},parameters:{controls:{include:["showTrailingAndLeadingDates"]}},argTypes:{showTrailingAndLeadingDates:{control:"boolean"}}},T={render:n=>e.jsx(ye,{...n}),args:{rowAutoHeight:!0,ignoreWhitespace:!0},parameters:{controls:{include:["rowAutoHeight","ignoreWhitespace"]}},argTypes:{rowAutoHeight:{control:"boolean"},ignoreWhitespace:{control:"boolean"}}},W={render:()=>e.jsx(je,{}),args:{}},H={render:()=>e.jsx(Se,{}),args:{}};var M,E,V;j.parameters={...j.parameters,docs:{...(M=j.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  render: args => <TimeScaleSample {...args} />,
+  args: {
+    interval: 60,
+    slotCount: 2,
+    enable: true
+  },
+  parameters: {
+    controls: {
+      include: ['interval', 'slotCount', 'enable']
+    }
+  },
+  argTypes: {
+    interval: {
+      control: 'select',
+      options: [30, 60, 90, 120, 150, 180, 240, 300, 720]
+    },
+    slotCount: {
+      control: 'select',
+      options: [1, 2, 3, 4, 5, 6]
+    },
+    enable: {
+      console: 'boolean'
+    }
+  }
+}`,...(V=(E=j.parameters)==null?void 0:E.docs)==null?void 0:V.source}}};var F,R,_;S.parameters={...S.parameters,docs:{...(F=S.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  render: args => <HeaderBarSample {...args} />,
+  args: {
+    showHeaderBar: true
+  },
+  parameters: {
+    controls: {
+      include: ['showHeaderBar']
+    }
+  },
+  argTypes: {
+    showHeaderBar: {
+      console: 'boolean'
+    }
+  }
+}`,...(_=(R=S.parameters)==null?void 0:R.docs)==null?void 0:_.source}}};var z,P,q;D.parameters={...D.parameters,docs:{...(z=D.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  render: args => <WorkDaysSample {...args} />,
+  args: {
+    workingDays: workDaysOptions[0].text,
+    firstDayOfWeek: dayOfWeekOptions[0].text,
+    showWeekend: true
+  },
+  parameters: {
+    controls: {
+      include: ['workingDays', 'firstDayOfWeek', 'showWeekend']
+    }
+  },
+  argTypes: {
+    workingDays: {
+      control: 'select',
+      options: workDaysOptions.map(o => o.text)
+    },
+    firstDayOfWeek: {
+      control: 'select',
+      options: dayOfWeekOptions.map(o => o.text)
+    },
+    showWeekend: {
+      control: 'boolean'
+    }
+  }
+}`,...(q=(P=D.parameters)==null?void 0:P.docs)==null?void 0:q.source}}};var G,I,J;v.parameters={...v.parameters,docs:{...(G=v.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  render: args => <WorkHoursSample {...args} />,
+  args: {
+    highlight: true
+  },
+  parameters: {
+    controls: {
+      include: ['highlight', 'start', 'end']
+    }
+  },
+  argTypes: {
+    highlight: {
+      control: 'boolean'
+    }
+  }
+}`,...(J=(I=v.parameters)==null?void 0:I.docs)==null?void 0:J.source}}};var K,Q,U;y.parameters={...y.parameters,docs:{...(K=y.parameters)==null?void 0:K.docs,source:{originalSource:`{
+  render: args => <DailyHourLimitSample {...args} />,
+  parameters: {
+    controls: {
+      include: ['startHour', 'endHour']
+    }
+  }
+}`,...(U=(Q=y.parameters)==null?void 0:Q.docs)==null?void 0:U.source}}};var X,Y,Z;f.parameters={...f.parameters,docs:{...(X=f.parameters)==null?void 0:X.docs,source:{originalSource:`{
+  render: args => <NumberOfWeeksSample {...args} />,
+  args: {
+    numberOfWeeks: 6
+  },
+  parameters: {
+    controls: {
+      include: ['numberOfWeeks']
+    }
+  },
+  argTypes: {
+    numberOfWeeks: {
+      control: 'select',
+      options: [1, 2, 3, 4, 5, 6]
+    }
+  }
+}`,...(Z=(Y=f.parameters)==null?void 0:Y.docs)==null?void 0:Z.source}}};var $,ee,ne;k.parameters={...k.parameters,docs:{...($=k.parameters)==null?void 0:$.docs,source:{originalSource:`{
+  render: args => <TrailingLeadingDatesSample {...args} />,
+  args: {
+    showTrailingAndLeadingDates: true
+  },
+  parameters: {
+    controls: {
+      include: ['showTrailingAndLeadingDates']
+    }
+  },
+  argTypes: {
+    showTrailingAndLeadingDates: {
+      control: 'boolean'
+    }
+  }
+}`,...(ne=(ee=k.parameters)==null?void 0:ee.docs)==null?void 0:ne.source}}};var te,se,ae;T.parameters={...T.parameters,docs:{...(te=T.parameters)==null?void 0:te.docs,source:{originalSource:`{
+  render: args => <RowAutoHeightSample {...args} />,
+  args: {
+    rowAutoHeight: true,
+    ignoreWhitespace: true
+  },
+  parameters: {
+    controls: {
+      include: ['rowAutoHeight', 'ignoreWhitespace']
+    }
+  },
+  argTypes: {
+    rowAutoHeight: {
+      control: 'boolean'
+    },
+    ignoreWhitespace: {
+      control: 'boolean'
+    }
+  }
+}`,...(ae=(se=T.parameters)==null?void 0:se.docs)==null?void 0:ae.source}}};var re,oe,le;W.parameters={...W.parameters,docs:{...(re=W.parameters)==null?void 0:re.docs,source:{originalSource:`{
+  render: () => <CellDimensionSample />,
+  args: {}
+}`,...(le=(oe=W.parameters)==null?void 0:oe.docs)==null?void 0:le.source}}};var ce,ie,de;H.parameters={...H.parameters,docs:{...(ce=H.parameters)==null?void 0:ce.docs,source:{originalSource:`{
+  render: () => <ReadOnlyEventsSample />,
+  args: {}
+}`,...(de=(ie=H.parameters)==null?void 0:ie.docs)==null?void 0:de.source}}};const Ne=["TimeScale","HeaderBar","WorkDays","WorkHours","DailyHourLimit","NumberOfWeeks","TrailingLeadingDates","RowAutoHeight","CellDimension","ReadOnlyEvents"];export{W as CellDimension,y as DailyHourLimit,S as HeaderBar,f as NumberOfWeeks,H as ReadOnlyEvents,T as RowAutoHeight,j as TimeScale,k as TrailingLeadingDates,D as WorkDays,v as WorkHours,Ne as __namedExportsOrder,Oe as default};
